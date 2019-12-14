8f1df1372bc6f78bd3e0753558fa27f0736caf49
4342315yc <4342315yc@gmail.com>
2019-12-14 19:59:08 +1100

    added demo file for exact algorithm

---
+++ Exact/demo_exact.py
@@ -0,0 +1,26 @@
+'''
+Implementation of some exact algorithms.
+No optimisation yet.
+CVRP
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import class_info
+from matplotlib.animation import FuncAnimation
+    
+def plot_solutions():
+    None
+    
+def plot_progress():
+    None
+    
+def demo(ps=True, pp=False):
+    
+    if ps:
+        plot_solutions(solution)
+    
+    None
+    
+if __name__ == '__main__':
+    demo()
