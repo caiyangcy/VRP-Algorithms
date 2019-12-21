ac6e225d5c5b70e2c345c74570c5f6801d8b6bce
4342315yc <4342315yc@gmail.com>
2019-12-21 18:16:06 +1100

    Update Exact algorithms

--- Exact/class_info.py
+++ Exact/class_info.py
@@ -8,75 +8,78 @@ CVRP
 import time
 from scipy.spatial import distance_matrix
 import itertools
+import numpy as np
 
 class CVRP_exact:
-    
-    depot, customers, demand = 0, 0
+    # depot and customers should be a 2d array
+    depot, customers, demand = 0, 0, 0
     split_delivery = False
     vehicle = None
     
-    def __init__(self, depot, customers, demand, split):
+    def __init__(self, depot, customers, demand):
         self.depot = depot
         self.customers = customers
         self.demand = demand
-        self.split_delivery = split
+#        self.split_delivery = split
         
-    def fit(v):
+    def fit(self, v):
         '''
         v: The vehicle used to solve the current delivery. It contains info on numbers and capacities.
         '''
-        if vehicle.capacity < demand.sum() and not split_delivery:
-            raise Exception("The capacity of vehicle is smaller than the maximum demand")
-            
-        else:
-            vehicle = v
-       
+        self.vehicle = v
         
-    def solve():
+        if self.vehicle.capacity < self.demand.max() and not self.split_delivery:
+            raise Exception("The capacity of vehicle is smaller than the maximum demand")
+            self.vehicle = None
+            
+        
+        
+    def solve(self):
         '''
         solver = branch and bound(bnb) or branch and cut(bnc)
         '''
-        if vehicle == None:
+        if self.vehicle == None:
             raise Exception("No vehicle info found")
         
         print('Start solving')
         
         start_time = time.time()
         
+        depot, customers = self.depot, self.customers
+        
         data = np.concatenate((depot, customers), axis=0)
         dm = distance_matrix(data, data)
-        
         solution = []
-        best_distance = np.sum(dm[0])
+        best_distance = np.inf
         
         choices = list(itertools.permutations(np.arange(1, customers.shape[0]+1, 1)))
-        
+        choices = choices[:len(choices)//2]
         for choice in choices:
-            total_distance = 0
-            
-            total_distance += dm[0, choice[0]-1] # distance between depot and the first customer
-            for idx in range(len(choice)-1):
-                total_distance += dm[idx-1, idx]
-            total_distance += dm[choice[-1]-1, 0]
-            
-            if total_distance > best_distance:
+  
+            total_distance = dm[0, choice[0]] # from depot to first customer
+
+            for idx in range(1, len(choice)):
+                total_distance += dm[choice[idx-1], choice[idx]]
+            total_distance += dm[choice[-1], 0] # from last customer to depot
+            if total_distance < best_distance:
                 best_distance = total_distance
                 solution = choice
             
         end_time = time.time()
-        print('Finished solving, with total time %s mins ' % ((end_time - start_time)/60))
+        print('\nFinished solving, with total time %s mins ' % ((end_time - start_time)/60))
         
-        return solution
+        solution = np.array(solution)
+        print('solution: ', solution)
+        return customers[solution-1], solution, best_distance
         
-        
-class vehicle:
+class Vehicle:
     '''
     current;y, number of vehicles is limited to 1
     '''
     capacity, num = 0, 0
     
     def __init__(self, capacity, num=1):
-        assert capacity > 0 and num > 0 and num%1 == 0, "invalid vehicle info"
+        assert capacity.min() > 0 and num > 0 and num%1 == 0, "invalid vehicle info"
         self.capacity = capacity
         self.num = num
         
--- Exact/demo_exact.py
+++ Exact/demo_exact.py
@@ -6,8 +6,10 @@ CVRP
 
 import numpy as np
 import matplotlib.pyplot as plt
-import class_info
+import class_info as Exact
 from matplotlib.animation import FuncAnimation
+
+
     
 def plot_solutions():
     None
@@ -18,9 +20,29 @@ def plot_progress():
 def demo(ps=True, pp=False):
     
     if ps:
-        plot_solutions(solution)
+        print()
+#        plot_solutions(solution)
     
     None
     
 if __name__ == '__main__':
-    demo()
+    
+    depot = np.array([[0,0]])
+#    customers = np.array([[1,1], [1,-1], [-1,1], [-1,-1]])
+    customers = np.array([[1,1], [1,2], [-1,2]])
+    demand = np.array([1,2,3,4])
+    
+    cvrp = Exact.CVRP_exact(depot, customers, demand)
+    
+    capacity = np.array([15])
+    num_vehicles = 1
+    vehicle = Exact.Vehicle(capacity, num_vehicles)
+    
+    cvrp.fit(vehicle)
+    
+    customer_order, solution, shortest_distance = cvrp.solve()
+    
+    print('\ncustomer visiting order:\n ', customer_order)
+    print('\nsolution: ', solution)
+    print('\nshortest_distance: ', shortest_distance)
+    
